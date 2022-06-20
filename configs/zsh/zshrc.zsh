81c1044b060a86489f535182c65d6d13ba460fe9
beams <makrovchansky@gmail.com>
2022-06-20 11:23:06 +0300

    zsh plugins

--- configs/zsh/zshrc.zsh
+++ configs/zsh/zshrc.zsh
@@ -1,8 +1,32 @@
-#Prompt
+# Prompt
 PS1="%F{green}%B%~/%b%f "
 
-#Exports 
+# Exports 
 export PATH="$HOME/.local/bin/:$PATH"
+
+#### Added by Zinit's installer
+if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+        print -P "%F{33} %F{34}Installation successful.%f%b" || \
+        print -P "%F{160} The clone has failed.%f%b"
+fi
+
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+### End of Zinit's installer chunk Start of Zinit installer's chunk
+
+# OMZ libs
+zinit snippet OMZ::lib/grep.zsh
+zinit snippet OMZ::lib/history.zsh
+zinit snippet OMZ::lib/completion.zsh
+
+# OMZ Plugins
+zinit snippet OMZ::plugins/git/git.plugin.zsh
+zinit snippet OMZ::plugins/systemd/systemd.plugin.zsh
+zinit snippet OMZ::plugins/sudo/sudo.plugin.zsh
 
 # Aliases
 alias l='lsd -lh --group-dirs first'
@@ -39,4 +63,6 @@ bindkey "^A" beginning-of-line
 bindkey "^Q" end-of-line
 bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
+
+
 
